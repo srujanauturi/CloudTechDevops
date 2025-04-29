1f70f23c05189deabab31ad9fd48e9f41dc208f4
srujanauturi <srujanauturi@gmail.com>
2025-04-29 22:56:34 +0530

    Update backend.tf

--- eks-terraform/backend.tf
+++ eks-terraform/backend.tf
@@ -7,9 +7,9 @@ terraform {
   }
 
   backend "s3" {
-    bucket = "veerasir22"
-    key    = "eks/terraform.tfstate"
-    region = "ap-southeast-2"
+    bucket = "projecteksbucket"
+    key    = "k8/eks/terraform.tfstate"
+    region = "us-east-1"
   }
 
   required_version = ">= 1.6.3"
